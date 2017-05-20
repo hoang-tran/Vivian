05822c45f111c345940b0d1b0c0347c380245d67
Hoang Tran <hoangtx.master@gmail.com>
2017-05-20 13:43:45 +0800

    Add first UI test

---
+++ VivianUITests/KIF+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  KIF+Extension.swift
+//  Vivian
+//
+//  Created by Hoang Personal on 20/5/17.
+//  Copyright © 2017 Hoang. All rights reserved.
+//
+
+import KIF
+
+extension XCTestCase {
+  func tester(file : String = #file, _ line : Int = #line) -> KIFUITestActor {
+    return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+  }
+  
+  func system(file : String = #file, _ line : Int = #line) -> KIFSystemTestActor {
+    return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+  }
+}
+
+extension KIFTestActor {
+  func tester(file : String = #file, _ line : Int = #line) -> KIFUITestActor {
+    return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+  }
+  
+  func system(file : String = #file, _ line : Int = #line) -> KIFSystemTestActor {
+    return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+  }
+}
